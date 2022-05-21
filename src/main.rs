b96d37209645972e2d1a5583e594fe01b74729f5
Philippe Voinov <philippevoinov@gmail.com>
2022-05-21 04:00:31 +0200

    Speculation (but it doesn't work with this server)

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,7 @@ impl fmt::Display for Direction {
     }
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 struct PosWithWalls {
     x: i32,
     y: i32,
@@ -411,6 +411,7 @@ impl Strategy for DFSStrategy {
 
 struct ImprovedDFSStrategy {
     goal: (i32, i32),
+    inputs: HashMap<(i32, i32), PosWithWalls>,
     added_positions: HashSet<(i32, i32)>,
     stack: Vec<(i32, i32)>,
     path_from_root: Vec<Direction>,
@@ -424,6 +425,7 @@ impl ImprovedDFSStrategy {
     fn new(goal: (i32, i32), distances: &Vec<f32>, distances_size: (i32, i32)) -> Self {
         Self {
             goal,
+            inputs: HashMap::new(),
             added_positions: HashSet::new(),
             stack: Vec::new(),
             path_from_root: Vec::new(),
@@ -444,6 +446,17 @@ impl Strategy for ImprovedDFSStrategy {
     fn step(&mut self, old_pos: &PosWithWalls) -> Result<Decision> {
         if self.goal != (0, 0) {
             return Err(anyhow!("unsupported goal"));
+        }
+
+        let k = (old_pos.x, old_pos.y);
+        if let Some(old_input) = self.inputs.get(&k) {
+            if old_input != old_pos {
+                return Err(anyhow!(
+                    "received conflicting information about a visited position"
+                ));
+            }
+        } else {
+            self.inputs.insert((old_pos.x, old_pos.y), old_pos.clone());
         }
 
         let mut possible_dirs = old_pos.possible_dirs();
@@ -509,7 +522,7 @@ impl Strategy for ImprovedDFSStrategy {
                 .reverse();
             Ok(Decision {
                 direction: back_direction,
-                speculated_position: None,
+                speculated_position: self.inputs.get(&back_direction.offset(old_pos)).cloned(),
             })
         }
     }
@@ -548,6 +561,11 @@ fn run_round<S: Strategy, F: FnOnce((i32, i32)) -> S, R: Read, W: Write>(
         writer.write(&ClientMessage::Move {
             direction: decision.direction,
         })?;
+
+        if let Some(speculated_position) = decision.speculated_position {
+            pos = Some(speculated_position);
+            continue;
+        }
 
         loop {
             let msg = reader.read()?;
@@ -668,7 +686,15 @@ fn run_offline<
                 steps_until_goal += 1;
             }
             match strategy.step(&inputs_by_position[&pos]) {
-                Ok(Decision { direction, .. }) => pos = direction.offset(pos),
+                Ok(Decision {
+                    direction,
+                    speculated_position,
+                }) => {
+                    pos = direction.offset(pos);
+                    if speculated_position.is_some() {
+                        assert!(inputs_by_position.get(&pos).cloned() == speculated_position);
+                    }
+                }
                 Err(_) => break,
             }
             if pos == goal {
